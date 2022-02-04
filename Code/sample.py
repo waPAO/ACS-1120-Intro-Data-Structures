0419ff7af7e45bc524f4565feb4ba35472352bc6
Pao <pzsigua@gmail.com>
2022-02-04 14:15:18 -0800

    Added sample.py

---
+++ Code/sample.py
@@ -0,0 +1,28 @@
+from histogram import histogram_dictionary
+import random
+
+def random_word(histogram: dict) -> str:
+    words = [word for word in histogram for i in range(histogram[word])]
+    dart = random.randint(0, len(words) - 1)
+
+    return words[dart]
+
+
+def track_count(loops: int, histogram: dict) -> dict:
+    histo_copy = histogram
+    words = [word for word in histogram for i in range(histogram[word])]
+    for word in histo_copy:
+        histo_copy[word] = 0
+
+    for i in range(loops):
+        dart = random.randint(0, len(words)-1)
+        word = words[dart]
+        histo_copy[word] += 1
+    return histo_copy
+
+
+
+if __name__ == '__main__':
+    histogram = histogram_dictionary('oneFish.txt')
+    print(random_word(histogram))
+    print(track_count(1000000, histogram))
