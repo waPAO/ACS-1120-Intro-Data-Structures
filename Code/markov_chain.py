e7ec90f35cd81884b7d5100ae7eaad771b32cc0d
Pao <pzsigua@gmail.com>
2022-02-14 13:46:43 -0800

    Added markov chain function

---
+++ Code/markov_chain.py
@@ -0,0 +1,12 @@
+from dictogram import Dictogram
+
+def build(word_list):
+    markovchain = {}
+    for index in range(len(word_list)-1):
+        firstword = word_list[index]
+        secondword = word_list[index+1]
+        if firstword not in markovchain:
+            markovchain[firstword] = Dictogram()
+            markovchain[firstword].add_count(secondword)
+    print(markovchain)
+    return markovchain
