ceef2dd54f5d31f16d4d03ea1ab727f57c30d207
Pao <pzsigua@gmail.com>
2022-03-06 19:16:25 -0800

    updated tokenizer to create word list

--- Code/tokens.py
+++ Code/tokens.py
@@ -2,6 +2,8 @@ import re
 
 def tokenize(corpus):
     final_text = remove_punctuation(remove_actions(remove_names(corpus)))
+    final_text = final_text.lower()
+    final_text = text_to_list(final_text)
     return final_text
 
 def remove_names(corpus):
@@ -17,6 +19,11 @@ def remove_punctuation(corpus):
     no_punc = re.sub('[^\w\s*\']', '', corpus)
     return no_punc
 
+def text_to_list(corpus):
+    split_text = re.split('\s+', corpus)
+    if '' in split_text:
+        split_text.remove('')
+    return split_text
 
 if __name__=='__main__':
     text = open('tester.txt').read()
