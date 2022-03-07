d7378647e8a076df76c9ea1fca22b4feb2cc32ec
Pao <pzsigua@gmail.com>
2022-03-06 21:06:16 -0800

    created twitter.py

---
+++ Code/twitter.py
@@ -0,0 +1,20 @@
+import os
+import dotenv
+from requests_oauthlib import OAuth1Session
+
+dotenv.load_dotenv('.env')
+
+consumer_key = os.environ.get('TWITTER_CONSUMER_KEY')
+consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET')
+access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
+access_token_secret = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+
+session = OAuth1Session(consumer_key, client_secret=consumer_secret, resource_owner_key=access_token, resource_owner_secret=access_token_secret)
+# The URL endpoint to update a status (i.e. tweet)
+url = 'https://api.twitter.com/2/tweets'
+
+def tweet(status):
+    # The URL endpoint to update a status (i.e. tweet)  
+    resp = session.post(url, json={ 'text': status })
+    return resp.text
+
